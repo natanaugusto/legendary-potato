d6bfcc353bcde5072b29cf7cabcb857429bbde7f
Natan Augusto <natanaugusto@gmail.com>
2022-09-14 19:39:47 -0300

    Starting with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsController } from './cars/cars.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '172.17.0.3',
+      port: 3306,
+      username: 'genr',
+      password: 'password',
+      database: 'legendary_potato',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CarsController],
   providers: [AppService],
 })
